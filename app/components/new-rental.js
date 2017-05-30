dd722abc3f4641573105ffc2c9576d9e37c7c092
Jason Ainsworth and James Higgins <james@>
2017-05-30 14:22:02 -0700

    Add save rental units

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -6,18 +6,21 @@ export default Ember.Component.extend({
    rentalFormShow() {
      this.set('addNewRental', true);
    },
-   saveRental1() {
+      saveRental1() {
        var params = {
-         owner: this.get('owner') ? this.get('owner') : "mickymouse",
-         city: this.get('city'),
+         owner: this.get('owner') ? this.get('owner') : "",
+         number: this.get('number') ? this.get('number') : "",
+         email: this.get('email') ? this.get('email') : "",
+         address: this.get('address') ? this.get('address') : "",
+         city: this.get('city') ? this.get('city') : "",
          type: this.get('type') ? this.get('type') : "",
-         image: this.get('image') ? this.get('image') : "google.com",
+         image: this.get('image') ? this.get('image') : "",
          bedrooms: this.get('bedrooms') ? this.get('bedrooms') : "",
+         bathrooms: this.get('bathrooms') ? this.get('bathrooms') : "",
+         year: this.get('year') ? this.get('year') : ""
        };
        this.set('addNewRental', false);
        this.sendAction('saveRental2', params);
      }
-
-
- }
+   }
 });
